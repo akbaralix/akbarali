ff156ef391aa364194d251169e3909892ca9bb50
Your Name <you@example.com>
2026-06-11 01:07:33 +0500

    m

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/akbarali/",
   plugins: [react()],
   build: {
     outDir: "docs",
-    base: "/akbarai/",
   },
 });
