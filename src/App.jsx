82c43bc58f555fb0c6f4cb5545a47b26a2a70dd0
Your Name <you@example.com>
2026-07-02 00:08:37 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import Button from "./components/Button";
 import AppRoutes from "./routes";
 import Navbar from "./pages/navbar/navbar";
 import ScrolToTop from "./components/ScrolToTop";
+import Lenis from "lenis";
 import "./App.css";
+import { useEffect } from "react";
 
 function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="App">
       <ScrolToTop />
